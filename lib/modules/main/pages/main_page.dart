a86677fe525532595313f817065d803c69375368
mohammad <m.deheghanifard@gmail.com>
2024-05-11 09:58:26 +0330

    impl DrawerItem custom widget

--- lib/modules/main/pages/main_page.dart
+++ lib/modules/main/pages/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_panel/core/resources/assets_path.dart';
 import 'package:flutter_admin_panel/core/resources/constants.dart';
+import 'package:flutter_admin_panel/modules/main/widgets/drawer_item.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
@@ -16,29 +17,7 @@ class MainPage extends StatelessWidget {
           children: [
             // DashBoard
             Expanded(
-                child: Container(
-                  color: context.theme.colorScheme.secondary,
-                  height: double.infinity,
-                  margin: const EdgeInsets.all(defaultPadding),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        // logo
-                        const FlutterLogo(size: 64,),
-                        ListTile(
-                          onTap: () {},
-                          title: const Text("لیست محصولات"),
-                          horizontalTitleGap: 8,
-                          leading: SvgPicture.asset(
-                            Assets.productListIcon,
-                            height: 16,
-                            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
+                child: DrawerWidget()
             ),
             // main card
             Expanded(
@@ -53,3 +32,32 @@ class MainPage extends StatelessWidget {
     );
   }
 }
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: context.theme.colorScheme.secondary,
+      height: double.infinity,
+      margin: const EdgeInsets.all(defaultPadding),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            // logo
+            const FlutterLogo(size: 64,),
+            DrawerItem(
+              onTap: () {},
+              title: "لیست محصولات",
+              svgPath: Assets.productListIcon,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/modules/main/widgets/drawer_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DrawerItem extends StatelessWidget {
+  const DrawerItem({
+    super.key, required this.title, required this.svgPath, required this.onTap,
+  });
+
+  final String title,svgPath;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTap,
+      title:  Text(title),
+      horizontalTitleGap: 8,
+      leading: SvgPicture.asset(
+        svgPath,
+        height: 16,
+        colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+      ),
+    );
+  }
+}
