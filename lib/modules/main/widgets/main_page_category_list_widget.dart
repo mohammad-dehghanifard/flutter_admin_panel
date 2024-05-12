5ad19081898d7700ea61ecc45c10553174bb6848
mohammad <m.deheghanifard@gmail.com>
2024-05-12 10:24:35 +0330

    completed show category widget in main screen

--- lib/modules/main/widgets/main_page_category_list_widget.dart
+++ lib/modules/main/widgets/main_page_category_list_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_admin_panel/core/fake_data.dart';
 import 'package:flutter_admin_panel/core/resources/constants.dart';
+import 'package:flutter_admin_panel/modules/main/data/models/category_model.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_utils/get_utils.dart';
 
@@ -11,20 +13,46 @@ class MainPageCategoryListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Wrap(
-      children: List.generate(4, (index) => Column(
+      children: List.generate(FakeData.categories.take(4).length, (index) {
+        final CategoryModel category = FakeData.categories[index];
+        return Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
             margin: EdgeInsets.only(top: 18.h,left: 4.w),
+            padding: const EdgeInsets.all(defaultPadding),
             width: 45.w,
             height: 160.h,
             decoration: BoxDecoration(
                 color: context.theme.colorScheme.secondary,
                 borderRadius: BorderRadius.circular(defaultRadius)
             ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // category image
+                Image.asset(category.image,height: 78.h),
+                //SizedBox(height: 8.h),
+                // category title
+                Text(category.title,maxLines: 1,overflow: TextOverflow.ellipsis,),
+                //SizedBox(height: 8.h),
+                // product count
+                RichText(
+                    text: TextSpan(
+                      text: "تعداد محصولات :  ",style: context.textTheme.bodySmall,
+                      children: [
+                        TextSpan(text: category.productCount.toString(),style: context.textTheme.bodyMedium!.apply(color: primaryColor ))
+                      ]
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                )
+              ],
+            ),
           ),
         ],
-      )),
+      );
+      }),
     );
   }
 }
